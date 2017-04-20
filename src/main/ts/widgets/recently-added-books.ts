2d394f839409dd2ca988d9509dd41b1f712a07ae
Matthew Lickei <mjlickei93@gmail.com>
2017-04-19 20:32:04 -0500

    Finished alpha version of recently added books widget.

--- src/main/ts/widgets/recently-added-books.ts
+++ src/main/ts/widgets/recently-added-books.ts
@@ -5,16 +5,26 @@ import {Widget} from "./widget";
 
 export class RecentlyAddedBooks extends Widget {
 
+    private $recentlyAddedBooks;
+
     constructor() {
         super();
 
+        this.$recentlyAddedBooks = $('.recently-added-books');
+
         let now = moment().format("YYYY-MM-DD HH:mm:ss");
         BookManager.getBooks({createdOn: now, limit: 10}, (books: Book[]) => {
-            //TODO create html and show on page
+            this.buildListing(books);
         });
     }
 
-    private buildListing(books:Book[]) {
+    private static buildBookHTML(book:Book) {
+        return `<li class="book recently-added-book"><div class="book-info"> <div class="book-title"><span class="attr-val">${book.title}</span></div><div class="book-author"><div class="attr-val"><span class="author-last">${book.authorLast}, </span><span class="author-first">${book.authorFirst}</span></div></div><div class="book-isbn"><span class="attr-lbl">ISBN </span><span class="attr-val">${book.isbn}</span></div></div></li>`;
+    }
 
+    private buildListing(books:Book[]) {
+        for(let book of books) {
+            this.$recentlyAddedBooks.append($(RecentlyAddedBooks.buildBookHTML(book)));
+        }
     }
 }
